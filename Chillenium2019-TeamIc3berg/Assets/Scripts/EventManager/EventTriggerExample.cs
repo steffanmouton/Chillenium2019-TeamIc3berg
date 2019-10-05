47e15188207453779305801dd037a239e8fcaa4f
Steffan Mouton <moutonsteffan@gmail.com>
2019-10-05 11:16:01 -0500

    Added EventManager scripts with Examples

---
+++ Chillenium2019-TeamIc3berg/Assets/Scripts/EventManager/EventTriggerExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTriggerExample : MonoBehaviour
+{
+    /*
+    void Update () {
+        if (Input.GetKeyDown ("q"))
+        {
+            EventManager.TriggerEvent ("test");
+        }
+
+        if (Input.GetKeyDown ("o"))
+        {
+            EventManager.TriggerEvent ("Spawn");
+        }
+
+        if (Input.GetKeyDown ("p"))
+        {
+            EventManager.TriggerEvent ("Destroy");
+        }
+
+        if (Input.GetKeyDown ("x"))
+        {
+            EventManager.TriggerEvent ("Junk");
+        }
+    }
+    */
+}
