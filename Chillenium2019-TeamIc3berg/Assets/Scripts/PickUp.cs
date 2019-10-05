9e9ecd79778faa6da84c58182faae95fdaf0487f
Xien Thomas <xien.thomas@gmail.com>
2019-10-05 08:45:54 -0500

    Made items in UI

---
+++ Chillenium2019-TeamIc3berg/Assets/Scripts/PickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+
+    private void start(){
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            for(int i = 0; i < inventory.slots.Length; i++){
+                if(inventory.isFull[i] == false){
+                    //item can be picked up
+                    //item is now equal true
+                    inventory.isFull[i] = true;
+                    break;
+                }else{
+                    //inventory is full
+                    break;
+                }
+            }
+        }
+    }
+}
