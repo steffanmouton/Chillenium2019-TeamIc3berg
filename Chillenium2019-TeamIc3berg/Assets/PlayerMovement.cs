4acfa6dd42973776f1554e4fe1b684b16e6086fa
Xien Thomas <xien.thomas@gmail.com>
2019-10-05 04:46:32 -0500

    Character movement

--- Chillenium2019-TeamIc3berg/Assets/PlayerMovement.cs
+++ Chillenium2019-TeamIc3berg/Assets/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //input
+        movement.x = Input.GetAxisRaw("HorizontalPlayerOne");
+        movement.y = Input.GetAxisRaw("VerticalPlayerOne");
+    }
+    void FixedUpdate(){
+        //movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
