3e55096d9a0980b710eb9e9f87b81013f0583157
Steffan Mouton <moutonsteffan@gmail.com>
2019-10-05 16:17:56 -0500

    Made player movement vector public, no more scale flip for main object

--- Chillenium2019-TeamIc3berg/Assets/PlayerMovement.cs
+++ Chillenium2019-TeamIc3berg/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     bool faceRight = true;
-    Vector2 movement;
+    public Vector2 movement;
 
     // Update is called once per frame
     void Update()
@@ -25,14 +25,14 @@ public class PlayerMovement : MonoBehaviour
             if(Input.GetAxisRaw("HorizontalPlayerOne") >0.5f & !faceRight){
                 faceRight = true;
                 Vector3 scale = transform.localScale;
-                scale.x *= -1;
+                
                 transform.localScale = scale;    
                 
             }else if(Input.GetAxisRaw("HorizontalPlayerOne") <-0.5f & faceRight){
                 
                 faceRight = false;
                 Vector3 scale = transform.localScale;
-                scale.x *= -1;
+                
                 transform.localScale = scale;
             }
             
