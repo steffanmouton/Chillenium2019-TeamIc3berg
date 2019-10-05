62d2d36008c8e833c24e734d8a2bc68d3f57fbcb
Xien Thomas <xien.thomas@gmail.com>
2019-10-05 07:56:29 -0500

    Finished split screen camera

---
+++ Chillenium2019-TeamIc3berg/Assets/cameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    Vector3 offset;
+    void FixedUpdate(){
+        Vector3 desirePos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desirePos, smoothSpeed);
+        transform.position = target.position;
+
+        transform.LookAt(target);
+    }
+}
