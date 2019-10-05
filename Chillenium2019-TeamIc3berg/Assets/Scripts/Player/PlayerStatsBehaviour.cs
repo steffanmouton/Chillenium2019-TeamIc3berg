465a10c33c5473c89b1a260e4e637b15d6abcca8
Steffan Mouton <moutonsteffan@gmail.com>
2019-10-05 16:16:56 -0500

    Updated PlayerAttackBehaviour Scripts
    Player now has a target circle ahead of them for attacking

---
+++ Chillenium2019-TeamIc3berg/Assets/Scripts/Player/PlayerStatsBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsBehaviour : MonoBehaviour
+{
+    public int hp = 1;
+
+    public bool isAlive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp < 1)
+        {
+            
+        }
+    }
+
+    private void Die()
+    {
+        
+    }
+
+    private void TakeDamage(int incomingDamage)
+    {
+        hp -= incomingDamage;
+    }
+}
