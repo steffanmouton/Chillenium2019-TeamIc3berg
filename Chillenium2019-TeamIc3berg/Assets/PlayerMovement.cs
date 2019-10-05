0a1e2109265c236662eb0715d83c7c00813cdf17
Xien Thomas <xien.thomas@gmail.com>
2019-10-05 05:22:08 -0500

    Full character movement

--- Chillenium2019-TeamIc3berg/Assets/PlayerMovement.cs
+++ Chillenium2019-TeamIc3berg/Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-
+    bool faceRight = true;
     Vector2 movement;
 
     // Update is called once per frame
@@ -18,6 +18,26 @@ public class PlayerMovement : MonoBehaviour
     }
     void FixedUpdate(){
         //movement
+        //if moving
+        
+        if(Input.GetAxisRaw("HorizontalPlayerOne")> 0.5f || Input.GetAxisRaw("HorizontalPlayerOne") < -0.5f){
+            
+            if(Input.GetAxisRaw("HorizontalPlayerOne") >0.5f & !faceRight){
+                faceRight = true;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;    
+                
+            }else if(Input.GetAxisRaw("HorizontalPlayerOne") <-0.5f & faceRight){
+                
+                faceRight = false;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+            
+        }
+       
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
