a60b66d14c69fd7f02aa4147463063b54806fd02
Steffan Mouton <moutonsteffan@gmail.com>
2019-10-04 23:38:51 -0500

    Added some sprites. Character not appearing in game view

--- Chillenium2019-TeamIc3berg/Assets/Scripts/PlayerControllerBehaviour.cs
+++ Chillenium2019-TeamIc3berg/Assets/Scripts/PlayerControllerBehaviour.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerBehaviour : MonoBehaviour
 {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private Vector2 moveVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
