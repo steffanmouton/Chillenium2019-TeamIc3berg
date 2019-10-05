ca382a1d37fb648baa5394f3220570633dbd510c
Xien Thomas <xien.thomas@gmail.com>
2019-10-05 08:29:55 -0500

    slots has space in code

--- Chillenium2019-TeamIc3berg/Assets/Inventory.cs
+++ Chillenium2019-TeamIc3berg/Assets/Inventory.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public bool[] isFull;
+    public GameObject[] slots;
 }
