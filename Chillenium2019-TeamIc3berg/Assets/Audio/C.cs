6c3a9eb8f95b7912a5a47fff1dd00911e5899fc4
Steffan Mouton <moutonsteffan@gmail.com>
2019-10-04 22:45:28 -0500

    Renamed Testscripts to Fix assembly errors

--- Chillenium2019-TeamIc3berg/Assets/Audio/C.cs
+++ Chillenium2019-TeamIc3berg/Assets/Audio/C.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class C : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
