433168b739321ff277e5ae2a5960ef68189f0539
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-10-06 03:05:47 -0500

    Add files via upload

---
+++ Chillenium2019-TeamIc3berg/Assets/Scripts/EventManager/Switches.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+    public bool activateBlock;
+    // starts false for some reason, until player interacts with plate
+    private void Start()
+    {
+        activateBlock = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        activateBlock = false;
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        activateBlock = true;
+    }
+}
