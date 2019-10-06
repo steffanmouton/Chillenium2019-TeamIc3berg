f6dd1dc9f6479b52abc15cc674bd604b55b19967
Xien Thomas <xien.thomas@gmail.com>
2019-10-06 15:08:20 -0500

    almost done, need music and title

--- Chillenium2019-TeamIc3berg/Assets/Scripts/LoadScreenOnClick.cs
+++ Chillenium2019-TeamIc3berg/Assets/Scripts/LoadScreenOnClick.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScreenOnClick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LoadByIndex(int sceneIndex){
+        SceneManager.LoadScene(sceneIndex);
     }
 }
